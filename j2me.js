04c12ddd6e0232b52799851e57b0ea176980e87c
DoiVaiGay <doivaigay1602@gmail.com>
2025-08-12 09:27:59 +0700

    Delete j2me.js

--- j2me.js
+++
@@ -1,4 +0,0 @@
-class J2ME {
-    loadJar(buf) { console.log("Loaded JAR with size", buf.byteLength); }
-    run() { document.body.innerHTML += '<h1 style="color:white;text-align:center">Giả lập J2ME đang chạy Hero.jar</h1>'; }
-}
